58f90a5211dcd6d4dc8133189b4063bc445d4fc5
a.shaukat@ssidecisions.com <a.shaukat@ssidecisions.com>
2023-08-17 06:41:33 +0500

    setup connection and make coffee table

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,8 +2,11 @@
 import { Module } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
+import { Coffee } from './entities/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([Coffee])],
 	providers: [CoffeesService],
 	controllers: [CoffeesController]
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,18 @@
 /* eslint-disable prettier/prettier */
-export class Coffee {
+
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Coffee { // sql table generated for this would be lower-case classname e.g coffee, can use @Entity('somedifferentname') for some different name
+	@PrimaryGeneratedColumn()
 	id: number;
+
+	@Column()
 	name: string;
+
+	@Column()
 	brand: string;
+
+	@Column('json', { nullable: true })
 	flavors: string[];
 }
