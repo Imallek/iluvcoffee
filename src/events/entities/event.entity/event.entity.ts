ec4b332db59f222e7ed42d236fd470d1f0e83068
a.shaukat@ssidecisions.com <a.shaukat@ssidecisions.com>
2023-08-20 14:21:51 +0500

    add index annotations on entity

--- src/events/entities/event.entity/event.entity.ts
+++ src/events/entities/event.entity/event.entity.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
+// @Index(['name', 'type'])  can create composite index like this, this annotation would have to be on entity level
 @Entity()
 export class Event {
 	@PrimaryGeneratedColumn()
@@ -9,6 +10,7 @@ export class Event {
 	@Column()
 	type: string;
 
+	@Index()				// Can create an index on any column if you think this table will be searched often using this index
 	@Column()
 	name: string;
 
